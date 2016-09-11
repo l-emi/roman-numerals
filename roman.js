f9bd9d978013771baef24c0c2ea724b120f97050
Lidiya <lemi.dev@gmail.com>
2016-09-11 19:15:01 +0100

    First Commit

---
+++ roman.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    
+  function convert() {
+        var num = $('.texty').val();
+        var ara = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 6, 5, 4, 1];
+        var rom = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+        var final = '';
+        for (var i = 0; i < rom.length; i++) {
+            while (num >= ara[i]) {
+                final += rom[i];
+                num -= ara[i];
+            }
+        }
+        $('.texty').val(final);
+    }
+    
+  $('#convert').click(function (){
+    convert();
+  });
+    
+});
+                
+                
