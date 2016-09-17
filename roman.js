d2566e1e3ecec5727ae9bba03e07cf33caf7e69c
Lidiya Nikolova <lemi.dev@gmail.com>
2016-09-17 12:15:57 +0100

    change "IX" to "VI" for 6

--- roman.js
+++ roman.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   function convert() {
         var num = $('.texty').val();
         var ara = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 6, 5, 4, 1];
-        var rom = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+        var rom = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'VI', 'V', 'IV', 'I'];
         var final = '';
         for (var i = 0; i < rom.length; i++) {
             while (num >= ara[i]) {
