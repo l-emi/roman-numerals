5cd594d59b4c1c5584997c7ccf83c31a361bb998
Lidiya <lemi.dev@gmail.com>
2016-10-03 15:11:33 +0100

    Comment out JS

--- roman.js
+++ roman.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     
+  /***************************
+  * Convert to Roman Numerals
+  ***************************/
   function convert() {
         var num = $('.texty').val();
-        var ara = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 6, 5, 4, 1];
-        var rom = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'VI', 'V', 'IV', 'I'];
+        var ara = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 6, 5, 4, 1]; //Arabic numbers
+        var rom = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'VI', 'V', 'IV', 'I']; //Roman numerals
         var final = '';
         for (var i = 0; i < rom.length; i++) {
             while (num >= ara[i]) {
